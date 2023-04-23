196253eaf1181388f164a909dd8a8cd06de6815b
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-04-23 12:10:15 +0530

    OOPS concept Intro

---
+++ Object Oriented JavaScript/oops.js
@@ -0,0 +1,28 @@
+/**
+ AGENDA OF TOPIC:-
+ * What is OOP? ( Object Oriented Programming) 
+ -> OOPS is a computer programming model that organizes software design around data , or 
+ objects rather than functions and logic. An object is define as a data feild that has unique
+ attributes and behaviors.
+ * Four pillars of OOPS ?
+ -> Encapsulation , Abstractions , Polymorphism and Inheritance
+ * What all we will covers
+ NOTE : JS is not Object Oriented Language , but we can write 
+ Object Oriented Code by using something called as Prototype Object.
+ */
+
+
+
+/**
+ OOPS Modules:- 
+ * This Keywords
+ * Constructor functions
+ * classes
+ * classical Inheritance
+ * Encapsulation
+ * Polymorphism
+ * Prototype Object
+ * Prototypal Inheritance
+ * Call Apply Bind
+ */
+
